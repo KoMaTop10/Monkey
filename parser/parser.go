b6d15695bda7d68f58e82e2850ea378b84da822a
KoMaTop10 <y.komachi10@gmail.com>
2023-10-10 20:32:13 +0900

    エラー解消

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"github.com/KoMaTop10/Monkey/ast"
 	"github.com/KoMaTop10/Monkey/lexer"
 	"github.com/KoMaTop10/Monkey/token"
+	"fmt"
 )
 
 type Parser struct {
@@ -11,14 +12,29 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t,p.peekToken.Type)
+	p.errors = append(p.errors,msg)
 }
 
 func (p *Parser) nextToken() {
@@ -35,6 +51,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 		if stmt != nil {
 			program.Statements = append(program.Statements,stmt)
 		}
+		p.nextToken()
 	}
 	return program
 }
@@ -82,6 +99,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	}else {
+		p.peekError(t)
 		return false
 	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,9 +1,11 @@
-package parser 
+package parser
+
 import (
+	"fmt"
 	"testing"
+
 	"github.com/KoMaTop10/Monkey/ast"
 	"github.com/KoMaTop10/Monkey/lexer"
-
 )
 
 func TestLetStatements(t *testing.T) {
@@ -12,10 +14,16 @@ func TestLetStatements(t *testing.T) {
 	let y = 10;
 	let foobar = 1234123;
 	`
+	fmt.Print("kokomadedekitayoooon")
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
+	checkParserErrors(t,p)
+
 	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
 		t.Fatalf("program.Statements does not contain 3 statements. got = %d",
 		len(program.Statements))
 	}
@@ -60,4 +68,17 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return 
+	}
+
+	t.Errorf("parser had %d errors",len(errors))
+	for _,msg := range errors {
+		t.Errorf("parser error: %q",msg)
+	}
+	t.FailNow()
 }
